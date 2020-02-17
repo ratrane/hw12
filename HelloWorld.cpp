bb06a78e353bd86517e4a2d7e304b3c886111bab
indiegameband <indiegameband@gmail.com>
2020-02-18 02:52:32 +0300

    Add project files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+//Call func std::cout
+void print()
+{
+    //wanna use standart dll for print
+    std::cout << "Hello Skillbox!\n";
+}
+/*
+1 set some int's
+2 HELLO WORLD!
+*/
+int main()
+{
+
+    int x = 100; 
+    3 + 7;
+    int y = x + 100;
+    
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+
+
+    std::cout << random;
+
+   
+}
